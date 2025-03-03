3849f0a2ba1b08fc6dc485f2f18984e8ef1fc594
Luigi Marino <emails@luigi-marino.com>
2025-03-03 19:30:48 +0000

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace Forge___Workout_Tracker
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new MainPage()) { Title = "Forge - Workout Tracker" };
+        }
+    }
+}
